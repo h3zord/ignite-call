7308e315929f55590a0b44cdbce577187d1dff6a
h3zord <lucaschmain@gmail.com>
2023-11-27 16:53:49 -0300

    feat: adding time intervals page

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -6,7 +6,7 @@ import { ConnectBox, ConnectItem, AuthError } from './styles'
 import { useRouter } from 'next/router'
 // import { api } from "../../../lib/axios"
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 

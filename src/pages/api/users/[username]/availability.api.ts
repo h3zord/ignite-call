73819233b85cc05e0adb242a6998d0491beec3af
Lucas <lucaschmain@gmail.com>
2024-02-07 09:44:02 -0300

    feat: blocking unavailable days

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -12,7 +12,6 @@ export default async function handler(
   }
 
   const username = String(req.query.username)
-
   const { date } = req.query
 
   if (!date) {
@@ -72,9 +71,13 @@ export default async function handler(
   })
 
   const availableTimes = possibleTimes.filter((time) => {
-    return !blockedTimes.some(
+    const isTimeBlocked = blockedTimes.some(
       (blockedTime) => blockedTime.date.getHours() === time,
     )
+
+    const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+
+    return !isTimeBlocked && !isTimeInPast
   })
 
   return res.json({ possibleTimes, availableTimes })
